0ac8007181155a615b5271d9840ec0f2590e002d
himansu <himansumallick502@gmail.com>
2025-06-16 15:59:30 +0530

    Updated local files

--- EMP/srtemp.java
+++ EMP/srtemp.java
@@ -8,9 +8,9 @@ public static void main(String[] args) {
       l.add(new Empsal(30000, "ansu"));
         l.add(new Empsal(20000, "money"));
         l.add(new Empsal(10000, "silu"));
-        l.add(new Empsal(5000, "anil"));
+        l.add(new Empsal(30000, "anil"));
 l.stream().sorted(((a,b)->{
-    Double val=a.sal-b.sal;
-    return val==0? Double.compare(b.sal,a.sal):a.name.compareTo(b.name);})).forEach(i->System.out.print(i.name+" "));
+    Double val=b.sal-a.sal;
+    return val==0?a.name.compareTo(b.name) :Double.compare(b.sal,a.sal);})).forEach(i->System.out.print(i.name+" "));
 
 }}
